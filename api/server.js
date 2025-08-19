1513cccfdb4146ec74715563f5d38fec8676f6f0
ryanguyen2 <rnguyen2102@gmail.com>
2025-08-19 13:28:50 -0400

    render deploy

--- api/server.js
+++ api/server.js
@@ -193,3 +193,8 @@ app.post("/explain", async (req, res) => {
 
 // ----
 app.listen(3000, () => console.log("API running on http://localhost:3000"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
